0f0d6bb271fecb47461c2228d4df1bc2b4829aec
avijoyhaldar2015 <avijoy.haldar2015@gmail.com>
2020-09-05 11:34:47 +0530

    $1

---
+++ src/compile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	string comm = argv[1];
+	comm="hanac.exe "+comm+".han";
+	system(comm.c_str());
+	comm = argv[1];
+	comm = "MinGW64\\bin\\g++.exe exec.cpp -o "+comm;
+	system(comm.c_str());
+}
