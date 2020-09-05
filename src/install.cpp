2efd69644b5442a30f9191d7f11c261799c0a0cb
Avijoy Haldar <avijoy.haldar2015@gmail.com>
2020-09-05 15:12:27 +0530

    Delete install.cpp

--- src/install.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	system("bin\\MinGW64\\bin\\g++.exe src\\translator-windows.cpp -o bin\\hanac.exe");
-	system("cls");
-	cout << "Hana Installation Successful!" << endl;
-}
