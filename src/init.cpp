2f8a586acdcb9e46b47445e09657e1ff7f62b686
Avijoy Haldar <avijoy.haldar2015@gmail.com>
2020-09-10 11:06:00 +0530

    Add files via upload

---
+++ src/init.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	system("g++ src/translator.cpp -o bin/hanac");
+	system("clear");
+	system("chmod +x bin/compile");
+	cout << "Hana Installed Successfully!" << endl;
+}
